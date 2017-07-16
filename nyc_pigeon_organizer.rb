7d84e1e9c1c6cd06c35365c2d1f963e28653f585
SchruteFarms23 <romanmullady@gmail.com>
2017-07-15 23:11:00 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+	new_hash = {}
+	data.each do |attr,value|
+		value.each do |stat,pigeon|
+			pigeon.each do |name|
+				new_hash[name] = {} unless new_hash[name]
+				if new_hash[name][attr]
+					new_hash[name][attr] << stat.to_s
+				else
+					new_hash[name][attr] = [stat.to_s]
+				end
+			end
+		end
+	end
+	new_hash
 end
